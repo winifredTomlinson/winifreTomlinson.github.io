a2ceeba4b9fdf42871da16c184c3addc2c33c431
winifredTomlinson <632861316@qq.com>
2016-04-19 12:07:42 +0800

    Update 文章索引.js

--- 文章索引.js
+++ 文章索引.js
@@ -1,13 +1,32 @@
 $(document).ready(function() {
-	$("#article-quoteJS1").mouseover(function(){$("#article-quoteJS1").css({"border":"#CCC 1px solid","box-shadow":"0 0 10px #999","cursor":"pointer"});$("#article-quoteJS1 pre").css("color","#de7d46");}).mouseout(function(){$("#article-quoteJS1").css({"border":"#fff 1px solid","box-shadow":"0 0 10px #fff"});$("#article-quoteJS1 pre").css("color","#333");});
-	$("#article-quoteJS2").mouseover(function(){$("#article-quoteJS2").css({"border":"#CCC 1px solid","box-shadow":"0 0 10px #999","cursor":"pointer"});$("#article-quoteJS2 pre").css("color","#de7d46");}).mouseout(function(){$("#article-quoteJS2").css({"border":"#fff 1px solid","box-shadow":"0 0 10px #fff"});$("#article-quoteJS2 pre").css("color","#333");});
-	$("#article-quoteHTML1").mouseover(function(){$("#article-quoteHTML1").css({"border":"#CCC 1px solid","box-shadow":"0 0 10px #999","cursor":"pointer"});$("#article-quoteHTML1 pre").css("color","#de7d46");}).mouseout(function(){$("#article-quoteHTML1").css({"border":"#fff 1px solid","box-shadow":"0 0 10px #fff"});$("#article-quoteHTML1 pre").css("color","#333");});
-	$("#article-quoteJS1").click(function(){window.open("性能调优.html","_self")})
-	$("#article-quoteJS2").click(function(){window.open("JS判断基本和引用类型.html","_self")})
-	$("#article-quoteHTML1").click(function(){window.open("说说那些HTML5标签.html","_self")})
-	$("#click-nav").hide();
-	$("#ion-navigate").mouseover(function(){$("#ion-navigate").css("color","#f7ce88");}).click(function(){$("#click-nav").slideDown("slow");}).mouseout(function(){$("#ion-navigate").css("color","#000");});
-		/*$(document).not("#click-nav").click(function(){$("#click-nav").slideUp("slow");$("#ion-navigate").show("");});   */
-	$("#ion-close").mouseover(function(){$("#ion-close").css("color","#f7ce88");}).mouseout(function(){$("#ion-close").css("color","#000");}).click(function(){$("#click-nav").slideUp("slow");$("#ion-navigate").show("");});
+	
+  $('.humberger').on('click', function(event) {
+    $(this).children(".menu-trigger").toggleClass("active");
+    if($(this).hasClass("erase")){
+      $(this).toggleClass("normal");
+      $(this).toggleClass("erase");
+    }else{
+      $(this).toggleClass("normal");
+      $(this).toggleClass("erase");
+    }
 
+    if ($('.slide_menu').hasClass('on_menu')) {
+      $('.slide_menu').removeClass('on_menu').stop().animate({"right":-300}, 400);
+    }else{
+      $('.slide_menu').addClass('on_menu').stop().animate({"right":0}, 400);
+    }
+  });
+  
+});
+$(".sp_humberger").click(function() {
+
+	$("#article-quote1").mouseover(function(){$("#article-quote1").css({"border":"#CCC 1px solid","box-shadow":"0 0 10px #999","cursor":"pointer"});$("#article-quote1 pre").css("color","#de7d46");}).mouseout(function(){$("#article-quote1").css({"border":"#fff 1px solid","box-shadow":"0 0 10px #fff"});$("#article-quote1 pre").css("color","#333");});
+	$("#article-quote2").mouseover(function(){$("#article-quote2").css({"border":"#CCC 1px solid","box-shadow":"0 0 10px #999","cursor":"pointer"});$("#article-quote2 pre").css("color","#de7d46");}).mouseout(function(){$("#article-quote2").css({"border":"#fff 1px solid","box-shadow":"0 0 10px #fff"});$("#article-quote2 pre").css("color","#333");});
+	$("#article-quote1").click(function(){window.open("说说那些HTML5标签.html","_self")})
+	$("#article-quote2").click(function(){window.open("#","_self")})
+
+	
+		/*$(document).not("#click-nav").click(function(){$("#click-nav").slideUp("slow");$("#ion-navigate").show("");});   */
+	
+	
 });
